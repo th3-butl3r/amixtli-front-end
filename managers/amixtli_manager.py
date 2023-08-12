f54aa42a5b199b7e6dd8ec93245d2cf8d46c7d34
Vidal Cardona <vrosas832@gmail.com>
2023-08-11 22:01:11 -0600

    feature: Validate reports page was implemented

--- managers/amixtli_manager.py
+++ managers/amixtli_manager.py
@@ -9,14 +9,19 @@ pass
 
 class AmixtliManager:
     @classmethod
-    def get_reports(self):
+    def get_reports(self, is_valid: bool = None):
         """Function to get all reports from API
 
         Returns:
             list: list of all reports
         """
         url = settings.AMIXTLI_API_REPORTS
-        response = requests.get(url)
+        if is_valid is not None:
+            params = {"is_valid": is_valid}
+            response = requests.get(url, params=params)
+        else:
+            response = requests.get(url)
+
         if response.status_code in [200, 201]:
             # TODO: AÑADIR LOGS
             response = json.loads(response.text)
