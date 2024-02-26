513b0d622514953f97de684371a840ffc678620e
Vidal Cardona <vrosas832@gmail.com>
2024-02-25 21:04:50 -0600

    feat: add admin access to valid reports page

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ class BaseConfig(BaseSettings):
     PROJECT_NAME: Optional[str] = "Amixtli-front-end"
     ENV_STATE: str = env_file["ENV_STATE"]
     AMIXTLI_API_REPORTS: str = env_file[f"{ENV_STATE.upper()}_AMIXTLI_API_REPORTS"]
+    ALLOWED_EMAILS: str = env_file[f"{ENV_STATE.upper()}_ALLOWED_EMAILS"]
+    SECRET_KEY: str = env_file[f"{ENV_STATE.upper()}_SECRET_KEY"]
 
 
 settings = BaseConfig()
