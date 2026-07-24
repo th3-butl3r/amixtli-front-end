2d29d4c4acd1e4c28ed1181f91c6ca96bf05cbb8
th3-butl3r <github-outsider.unknowing582@passinbox.com>
2026-07-23 18:35:22 -0600

    feat: reduce cost AWS

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -7,7 +7,7 @@ resource "aws_ecr_repository" "page_nuestroentorno" {
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = false
   }
 }
 
